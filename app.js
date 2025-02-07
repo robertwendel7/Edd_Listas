f4a22230c85b05dc2222560bdbd54c6b02eb06c3
Robert Wendel <134088034+robertwendel7@users.noreply.github.com>
2025-02-07 20:24:38 +0000

    primeiro commit

---
+++ app.js
@@ -0,0 +1,46 @@
+let items = []
+
+function append(element){
+    items.push(element)
+}
+function insert(position, element){
+    // items.splice(position, 0, element)
+    if(position > items.lenght || position < 0){
+        return 
+    }
+    
+    // realocar do fim ao 'position' os elementos
+    for(let i = items.length - 1; i >= position; i--){
+        items[i+1] = items[i]
+    }
+
+    // alocar o 'element' na 'position'
+    items[position] = element
+}
+function remove(element){
+
+}
+function removeAt(position){
+
+}
+function indexOt(element){
+
+}
+function size(){
+
+}
+function toString(){
+
+}
+function isEmpty(){
+
+}
+function print(){
+
+}
+function clear(){
+
+}
+function search(element){
+
+}
